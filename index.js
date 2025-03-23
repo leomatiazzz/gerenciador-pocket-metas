ed18959c3764940cf88dbb766d94091bb71452e1
leomatiazzz <leo21matias@hotmail.com>
2025-03-23 16:54:52 -0300

    apresenta metas realizadas

--- index.js
+++ index.js
@@ -45,6 +45,22 @@ const listarMetas = async () => {
   console.log("Meta(s) marcada(s) como concluída(s)");
 };
 
+const metasRealizadas = async () => {
+  const realizadas = metas.filter((meta) => {
+    return meta.checked;
+  });
+
+  if (realizadas.length == 0) {
+    console.log("Não existem metas realizadas! :(");
+    return;
+  }
+
+  await select({
+    message: "Metas realizadas",
+    choices: [...realizadas],
+  });
+};
+
 const start = async () => {
   while (true) {
     const opcao = await select({
@@ -57,6 +73,10 @@ const start = async () => {
         {
           name: "Listar metas",
           value: "listar",
+        },
+        {
+          name: "Metas realizadas",
+          value: "realizadas",
         },
         {
           name: "Sair",
@@ -72,7 +92,9 @@ const start = async () => {
         break;
       case "listar":
         await listarMetas();
-        console.log("Vamos listar");
+        break;
+      case "realizadas":
+        await metasRealizadas();
         break;
       case "sair":
         console.log("Até a próxima!");
